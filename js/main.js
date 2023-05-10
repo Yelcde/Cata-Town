deef4fa35320812ba0cb57e4f244f5969ad5e40f
Sammuel13 <sammluna13@gmail.com>
2023-05-10 15:09:47 -0300

    filter

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 import { writeOnTable } from './writeOnTable.js';
 
 const svgBairros = document.querySelector('.map-area');
-const paths = svgBairros.querySelectorAll('path');
+const paths = Array.from(svgBairros.querySelectorAll('path'));
 let lastClicked = undefined;
 
-paths.forEach((path) => {
-	if (path.id.slice(0,4) != 'path'){
-		path.style.fill = '#E0FFEB';
-		path.addEventListener('click', (event) => {
-			const clickedPath = event.target;
-			if (lastClicked) { lastClicked.style.fill = '#E0FFEB' };
-			lastClicked = clickedPath;
-			lastClicked.style.fill = '#9DE0AD'
-			writeOnTable(clickedPath.id)
-		});
-	};
+const filteredArr = paths.filter(path => path.id.slice(0,4) != 'path');
+
+filteredArr.forEach((path) => {
+	path.style.fill = '#E0FFEB';
+	path.addEventListener('click', (event) => {
+		const clickedPath = event.target;
+		if (lastClicked) { lastClicked.style.fill = '#E0FFEB' };
+		lastClicked = clickedPath;
+		lastClicked.style.fill = '#9DE0AD'
+		writeOnTable(clickedPath.id)
+	});
 });
