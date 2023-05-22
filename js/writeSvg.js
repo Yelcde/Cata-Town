93f669304df6abff5dd22ada694564c05dd63730
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-05-22 17:02:07 -0300

    "Finalizando" a responsividade do site

--- js/writeSvg.js
+++ js/writeSvg.js
@@ -11,8 +11,8 @@ divHtml.innerHTML = `
    xmlns:xlink="http://www.w3.org/1999/xlink"
    version="1.1"
    id="svg4496"
-   width="531.25385"
-   height="531.21527"
+   width="100%"
+   height="100%"
    viewBox="0 0 531.25386 531.21526">
   <metadata
      id="metadata4502">
